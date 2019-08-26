15e7ff349e5c5aed43306050143c02e9794731d0
kumarrau <RAUSHAN.KUMAR2@otis.COM>
2019-08-26 15:35:00 +0530

    Update the code for create, update and delete posts

--- untitled/settings.py
+++ untitled/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
